9578e40a6502c1f82e335c47e432674ed64bf060
Kaan Büyükkıvanç <k.buyukkivanc@gmail.com>
2023-12-09 23:20:45 +0300

    v2

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.weatherapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,7 +18,46 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        val zipcodeEditText : EditText = findViewById(R.id.zipcodeEditText)
+        val enterButton : Button = findViewById(R.id.enterButton)
+
+        enterButton.setOnClickListener {
+
+            val zipcode : String = zipcodeEditText.text.toString()
+
+            if(zipcode.length != 5){
+                Toast.makeText(this, "Enter a valid zipcode number", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, zipcode, Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
     }
+
+    override fun onStart(){
+        super.onStart()
+    }
+
+    override fun onResume(){
+        super.onResume()
+    }
+
+    override fun onPause(){
+        super.onPause()
+    }
+
+    override fun onStop(){
+        super.onStop()
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+    }
+
+
 }
 
