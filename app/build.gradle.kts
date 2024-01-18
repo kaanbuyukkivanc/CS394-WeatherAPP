e98b5617dfd7ada3b329dfb4744d70e56e6b7399
Kaan Büyükkıvanç <k.buyukkivanc@gmail.com>
2024-01-19 00:07:00 +0300

    Binding Adapter has been added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,8 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("com.google.firebase:firebase-firestore:24.10.0")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.10.0")
+    implementation("io.coil-kt:coil:1.1.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
--- app/src/main/java/com/example/weatherapp/details/WeatherDetailsFragment.kt
+++ app/src/main/java/com/example/weatherapp/details/WeatherDetailsFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.fragment.navArgs
+import com.example.weatherapp.R
 import com.example.weatherapp.TempDisplaySettingManager
+import com.example.weatherapp.data.Weather
 import com.example.weatherapp.databinding.FragmentWeatherDetailsBinding
 import com.example.weatherapp.formatTemplateForDisplay
 
@@ -15,32 +19,34 @@ class WeatherDetailsFragment : Fragment() {
 
     private val args : WeatherDetailsFragmentArgs by navArgs()
     private val viewModel = WeatherDetailsViewModel()
-    private var _binding : FragmentWeatherDetailsBinding? = null
-    private val binding get() = _binding!!
+    //private var _binding : FragmentWeatherDetailsBinding? = null
+    //private val binding get() = _binding!!
     private lateinit var tempDisplaySettingManager: TempDisplaySettingManager
+
+    private lateinit var binding: FragmentWeatherDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentWeatherDetailsBinding.inflate(inflater, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_weather_details, container, false)
         tempDisplaySettingManager = TempDisplaySettingManager(requireContext())
 
-        binding.tempDetails.text = formatTemplateForDisplay(args.temp, tempDisplaySettingManager.getTimeDisplaySetting())
-        binding.descriptionDetails.text = args.description
-        binding.dateDetailis.text = args.date
-        binding.ImageDetails.setImageResource(args.icon)
-
-
+        binding.weather = getWeatherFromArgs()
+        binding.tempDetails.setText(formatTemplateForDisplay(args.temp, tempDisplaySettingManager.getTimeDisplaySetting()))
 
         return binding.root
     }
-
+    private fun getWeatherFromArgs(): Weather {
+        return Weather(args.temp,args.description,args.date,args.icon) // Assuming 'weather' is the correct property in your args class
+    }
+/*
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewStateObserver = Observer<WeatherDetailsViewState> {
             viewState ->
+
             binding.dateDetailis.text = formatTemplateForDisplay(viewState.temp, tempDisplaySettingManager.getTimeDisplaySetting())
             binding.descriptionDetails.text = viewState.description
             binding.dateDetailis.text = viewState.date
@@ -54,7 +60,7 @@ class WeatherDetailsFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
+*/
 }
 
 
